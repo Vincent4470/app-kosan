e3dfad347a66c6249e8a4c007f57a83d680d8688
Vincent <vincent4470@example.com>
2025-01-28 12:03:02 +0700

    update

---
+++ app/Interfaces/BoardingHouseRepositoryInterface.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Interfaces;
+
+interface BoardingHouseRepositoryInterface
+{
+    public function getAllBoardingHouses($seacrh = null, $city = null, $category = null);
+
+    public function getPopularBoardingHouses($limit = 5);
+
+    public function getBoardingHouseByCitySlug($slug);
+
+    public function getBoardingHouseByCategorySlug($slug);
+
+    public function getBoardingHouseBySlug($slug);
+
+}
+
+
