4eed543593a43bb75f07b6e9fd85a85c78270d46
Vincent <vincent4470@example.com>
2025-02-10 08:03:29 +0700

    update

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -12,4 +12,17 @@ class HomeController extends Controller
     private CityRepositoryInterface $cityRepository;
     private CategoryRepositoryInterface $categoryRepository;
     private BoardingHouseRepositoryInterface $boardingHouseRepository;
+
+
+    public function  __construct(
+        CityRepositoryInterface $cityRepository,
+        CategoryRepositoryInterface $categoryRepository,
+        BoardingHouseRepositoryInterface $boardingHouseRepository
+    ) {
+        $this->cityRepository = $cityRepository;
+        $this->categoryRepository = $categoryRepository;
+        $this->boardingHouseRepository = $boardingHouseRepository;
+    }
+
+    public function index () {}
 }
