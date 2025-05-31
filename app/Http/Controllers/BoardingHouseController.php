ef50c502f00c8af116d3c74602bb858f69cc7733
Vincent <vincent4470@example.com>
2025-05-31 22:41:21 +0700

    update

--- app/Http/Controllers/BoardingHouseController.php
+++ app/Http/Controllers/BoardingHouseController.php
@@ -23,10 +23,32 @@ class BoardingHouseController extends Controller
         $this->categoryRepository = $categoryRepository;
         $this->boardingHouseRepository = $boardingHouseRepository;
     }
+
+    public function show($slug)
+    {
+        $boardingHouse = $this->boardingHouseRepository->getBoardingHouseBySlug($slug);
+        return view('pages.boarding-house.show', compact('boardingHouse'));
+    }
+
+    public function rooms($slug)
+    {
+        $boardingHouse = $this->boardingHouseRepository->getBoardingHouseBySlug($slug);
+        return view('pages.boarding-house.rooms', compact('boardingHouse'));
+    }
     public function find()
     {
         $categories = $this->categoryRepository->getAllCategories();
         $cities = $this->cityRepository->getAllCities();
         return view('pages.boarding-house.find', compact('categories', 'cities'));
+    }
+
+    public function findResults(Request $request)
+    {
+        $boardingHouses = $this->boardingHouseRepository->getAllBoardingHouses(
+            $request->search,
+            $request->city,
+            $request->category
+        );
+        return view('pages.boarding-house.index', compact('boardingHouses'));
     }
 }
